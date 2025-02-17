9b65502768471490291377a695fe58da9fbbd177
emogotka <74817471+uknowvincent@users.noreply.github.com>
2025-02-17 23:00:32 +0700

    Update javacode6.js

--- javacode6.js
+++ javacode6.js
@@ -1,9 +1,22 @@
-$(document).ready(function() {
-  var $slider = $('.landing .slider');
-  var maxItems = $('.item', $slider).length;
+var $slider = $('.landing .slider'),
+    maxItems = $('.item', $slider).length,
+    dragging = false,
+    tracking,
+    rightTracking;
 
-  // Инициализация основного слайдера
-  $slider.addClass('landing-left').slick({
+$sliderRight = $('.landing').clone().addClass('landing-right').appendTo($('.split-landing'));
+
+rightItems = $('.item', $sliderRight).toArray();
+reverseItems = rightItems.reverse();
+$('.slider', $sliderRight).html('');
+
+for (i = 0; i < maxItems; i++) {
+    $(reverseItems[i]).appendTo($('.slider', $sliderRight));
+}
+
+$slider.addClass('landing-left');
+
+$('.landing-left').slick({
     vertical: true,
     verticalSwiping: true,
     arrows: false,
@@ -11,18 +24,30 @@ $(document).ready(function() {
     dots: true,
     speed: 1000,
     cssEase: 'cubic-bezier(0.7, 0, 0.3, 1)'
-  });
+}).on('beforeChange', function(event, slick, currentSlide, nextSlide) {
+    if (currentSlide > nextSlide && nextSlide == 0 && currentSlide == maxItems - 1) {
+        $('.landing-right .slider').slick('slickGoTo', -1);
+        $('.landing-text').slick('slickGoTo', maxItems);
+        $('.landing-p').slick('slickGoTo', maxItems);
+    } else if (currentSlide < nextSlide && currentSlide == 0 && nextSlide == maxItems - 1) {
+        $('.landing-right .slider').slick('slickGoTo', maxItems);
+        $('.landing-text').slick('slickGoTo', -1);
+        $('.landing-p').slick('slickGoTo', -1);
+    } else {
+        $('.landing-right .slider').slick('slickGoTo', maxItems - 1 - nextSlide);
+        $('.landing-text').slick('slickGoTo', nextSlide);
+        $('.landing-p').slick('slickGoTo', nextSlide);
+    }
+}).on("mousewheel", function(event) {
+    event.preventDefault();
+    if (event.originalEvent.deltaY < 0) {
+        $(this).slick('slickPrev');
+    } else {
+        $(this).slick('slickNext');
+    }
+});
 
-  // Инициализация правого слайдера
-  var $sliderRight = $('.landing').clone().addClass('landing-right').appendTo($('.split-landing'));
-  var rightItems = $('.item', $sliderRight).toArray();
-  var reverseItems = rightItems.reverse();
-  $('.slider', $sliderRight).html('');
-  for (var i = 0; i < maxItems; i++) {
-    $(reverseItems[i]).appendTo($('.slider', $sliderRight));
-  }
-
-  $('.landing-right .slider').slick({
+$('.landing-right .slider').slick({
     swipe: false,
     vertical: true,
     arrows: false,
@@ -30,5 +55,23 @@ $(document).ready(function() {
     speed: 950,
     cssEase: 'cubic-bezier(0.7, 0, 0.3, 1)',
     initialSlide: maxItems - 1
-  });
+});
+
+// Слайдеры для текста
+$('.landing-text').slick({
+    swipe: false,
+    vertical: true,
+    arrows: false,
+    infinite: true,
+    speed: 900,
+    cssEase: 'cubic-bezier(0.7, 0, 0.3, 1)'
+});
+
+$('.landing-p').slick({
+    swipe: false,
+    vertical: true,
+    arrows: false,
+    infinite: true,
+    speed: 900,
+    cssEase: 'cubic-bezier(0.7, 0, 0.3, 1)'
 });
